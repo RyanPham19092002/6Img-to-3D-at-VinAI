d4b48d3ea454a27f3efa93c18e88ac8e29e73465
Phat Thanh Pham <122810752+RyanPham19092002@users.noreply.github.com>
2024-05-13 10:53:20 +0700

    Update dataset_eval.py

--- config/_base_/dataset_eval.py
+++ config/_base_/dataset_eval.py
@@ -1,41 +1,37 @@
 
 dataset_params = dict(
-    data_path = "/app/data/",
+    data_path = "data_NEO360",
     version = "triplane",
     train_data_loader = dict(
-        pickled = True,
+        pickled = True, # If True, first run 'python utils/pickles_generator.py --dataset-config <path to this file> --py-config <path to config.py>' to preprocess the dataset and save as pickles for faster data loading
         phase = "train",
         batch_size = 1,
         shuffle = True,
         num_workers = 12,
-        # town = ["Town03"],
-        town = ["Town01", "Town03", "Town04", "Town05", "Town06", "Town07", "Town10HD"],
-        # town = ["Town02"],
+        town = ["Town01", "Town03", "Town04"],
         weather  = ["ClearNoon"],
         vehicle = ["vehicle.tesla.invisible"],
         spawn_point = ["all"],
-        # spawn_point = [1,2],
         step = ["all"],
         selection = ["input_images", "sphere_dataset"],
         factor = 0.08,
         whole_image = True,
         num_imgs = 3,
         depth=True
-    ),
+    ), 
     val_data_loader = dict(
-        pickled = False,
-        phase = "test",
+        pickled = False, # True is unsupported
+        phase = "test", # "train", "test", "full"
         batch_size = 1,
         shuffle = False,
         num_workers = 12,
         town = ["Town02"],
         weather  = ["ClearNoon"],
         vehicle = ["vehicle.tesla.invisible"],
-        spawn_point = ["all"],
+        spawn_point = [10],
         step = ["all"],
         selection = ["input_images", "sphere_dataset"],
-        factor = 0.5,
+        factor = 0.25,
         depth = True,
     ),    
 )
-
