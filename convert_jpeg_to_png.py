7b059c0edd4363e9d536fb981b4caab837a7d5d3
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-05-16 14:43:07 +0000

    add_data_VinAi

---
+++ convert_jpeg_to_png.py
@@ -0,0 +1,14 @@
+import os
+from PIL import Image
+
+folder_path = "/root/VinAI/6Img-to-3D-at-VinAI/data_VinAI/Town05/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_0/sphere/transforms/sphere"
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.jpeg'):
+        img_path = os.path.join(folder_path, filename)
+        img = Image.open(img_path)
+        png_path = os.path.splitext(img_path)[0] + ".png"
+        
+        #img.save(png_path)
+        os.remove(img_path)
+        print(f"Converted {filename} to PNG format.")
