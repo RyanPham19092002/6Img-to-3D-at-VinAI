95f935b248c09c536401fffdb5cb34b655fce9ea
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-05-19 10:28:50 +0000

    continue_train bash

--- continue_train.sh
+++ continue_train.sh
@@ -1,2 +1,2 @@
 pkill python
-python train.py --py-config config/config.py --log-dir evaluation_results 
+python train.py --py-config config/config.py --log-dir evaluation_results --ckpt-path /root/VinAI/6Img-to-3D-at-VinAI/runs/May19_09-17-46_evaluation_results/models/model_latest.pth
