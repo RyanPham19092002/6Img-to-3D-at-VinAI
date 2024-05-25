0939ca1bed1341672e19c31095ef6e415800695b
Phat Thanh Pham <122810752+RyanPham19092002@users.noreply.github.com>
2024-05-25 11:43:47 +0700

    Update resize_img.py

--- resize_img.py
+++ resize_img.py
@@ -4,7 +4,7 @@ from PIL import Image
 for town in ["Town02", "Town05"]:
     for index in range(41,52):
         print("town - index---------------------", town, index)
-        folder_path = f"/root/VinAI/6Img-to-3D-at-VinAI/data_VinAI/{town}/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_{index}/sphere/transforms/sphere_dataset_log_depth"
+        folder_path = f"./VinAI/6Img-to-3D-at-VinAI/data_VinAI/{town}/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_{index}/sphere/transforms/sphere_dataset_log_depth"
 
         for filename in os.listdir(folder_path):
             if filename.endswith('.png') or filename.endswith('.jpeg'):
