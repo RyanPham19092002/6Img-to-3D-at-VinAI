707f0a2b95db38bc7140de8bb58927eb8bc52f33
Phat Thanh Pham <122810752+RyanPham19092002@users.noreply.github.com>
2024-05-25 20:04:24 +0700

    Update check_img_rgb_or_gray.py

--- check_img_rgb_or_gray.py
+++ check_img_rgb_or_gray.py
@@ -1,10 +1,8 @@
 from PIL import Image
 
-<<<<<<< HEAD
+
 depth_path = './data_VinAI_10scene_1600x928/Town05/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_50/sphere/transforms/sphere_dataset_log_depth/spherical_1.png'
-=======
-depth_path = './VinAI/6Img-to-3D-at-VinAI/data_VinAI_1600/Town05/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_0/sphere/transforms/sphere_dataset_log_depth/spherical_1.png'
->>>>>>> 2915764262959867fa18eaad97414b8be5178ba1
+
 depth_map = Image.open(depth_path)
 image_mode = depth_map.mode
 
