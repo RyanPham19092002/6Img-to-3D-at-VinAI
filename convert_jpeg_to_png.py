28231c5ae77aebcc024c8779e60a9cb43e3da29f
Phat Thanh Pham <122810752+RyanPham19092002@users.noreply.github.com>
2024-05-25 11:45:56 +0700

    Update convert_jpeg_to_png.py

--- convert_jpeg_to_png.py
+++ convert_jpeg_to_png.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-folder_path = "/root/VinAI/6Img-to-3D-at-VinAI/data_VinAI/Town05/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_0/sphere/transforms/sphere_dataset"
+folder_path = "./VinAI/6Img-to-3D-at-VinAI/data_VinAI/Town05/ClearNoon/vehicle.tesla.invisible/spawn_point_10/step_0/sphere/transforms/sphere_dataset"
 
 for filename in os.listdir(folder_path):
     if filename.endswith('.jpeg'):
