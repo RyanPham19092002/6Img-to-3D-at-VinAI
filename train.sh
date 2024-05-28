89ac77428fbc1bf06a821af899e97df15d85d0dc
Phat Thanh Pham <122810752+RyanPham19092002@users.noreply.github.com>
2024-05-28 17:35:46 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 pkill python
-python train.py --py-config config/config.py --log-dir multiply_25_data_1600x928_10scenes
+python train.py --py-config config/config.py --log-dir depth_raw_data_1600x928_10scenes_sphere_800x600
